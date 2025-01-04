89ad465aca4241e080366c1764331e9c1b458b56
Diego <Diegofla10@outlook.com>
2025-01-03 21:58:42 -0300

    .
    .

---
+++ main.js
@@ -0,0 +1,15 @@
+var traile = document.querySelector('.trailer-container');
+
+
+function popUp()
+{
+
+   traile.style.display = 'flex';
+
+}
+
+function out()
+    {
+        traile.style.display = 'none';
+
+    }
